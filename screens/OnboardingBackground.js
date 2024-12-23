89f6de8dc90db64fd2573259958351f95c3d11bb
Julianne Tomlinson <118313562+JewlzT@users.noreply.github.com>
2024-12-22 19:19:01 -0500

    Updated background screen for onboarding
    - created dynamic list for ethnicities/cultures

--- screens/OnboardingBackground.js
+++ screens/OnboardingBackground.js
@@ -1,12 +1,39 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList} from 'react-native';
+import { Bar } from 'react-native-progress';
 
 
 export default function OnboardingBackground({navigation}) {
+  // Create Input variable and culture list variable
+  const [inputText, setInputText] = useState('');
+  const [cultureList, setcultureList] = useState([]);
+
+  // Create function to add word to list
+  const handleAddWord = () => {
+    // Make sure user typed in a word
+    if (inputText.trim()){
+      setcultureList([...cultureList, inputText.trim()]);
+      setInputText('');
+    }
+  }
   return (
     <View style={styles.container}>
+      <Bar progress={0.33} width={300} color="#F28B0C" />
       <Text style={styles.titleText}>What is your background?</Text>
-      <Text style={styles.text}>Help us get to know you better so we can connect you more deeply with your roots.</Text>
+      <Text style={styles.text}>Type in your ethnicity or nationality</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter ethnicity/nationality"
+        value={inputText}
+        onChangeText={setInputText}
+        onSubmitEditing={handleAddWord}
+      />
+      <FlatList
+        data={cultureList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text style={styles.wordItem}>{item}</Text>}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,11 +44,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#8A860A',
     justifyContent: 'flex-start',
-    padding: 20
+    padding: 20,
+    paddingTop: 70
   },
   titleText: {
     color: '#fff',
-    fontSize: 50
+    fontSize: 50,
+    paddingTop: 30
   },
   text: {
     color: '#fff',
